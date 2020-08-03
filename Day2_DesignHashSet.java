783a78a55738ce2717eca8f34c5bf7cbdb1075d8
vigneshkarthick2606 <60441471+vigneshkarthick2606@users.noreply.github.com>
2020-08-03 13:32:04 +0530

    update optimal approach using seperate chaning

--- Day2_DesignHashSet.java
+++ Day2_DesignHashSet.java
@@ -29,6 +29,9 @@ Note:
 */
 --------------------------------------------------------------------------------------------------------------------------
 
+// This is naive approach
+// Need to use seperate Chaining to handle hash collision
+// List<Intger>
 class MyHashSet {
     
     int[] bucket;
@@ -56,3 +59,118 @@ class MyHashSet {
         return false;
     }
 }
+
+
+----------------------------------------------------------------------------------------------
+
+class MyHashSet {
+    
+    class Node {
+        int value;
+        Node next;
+        
+        public Node(int key){
+            value = key;
+            next = null;
+        }
+    }
+    
+    Node[] set;
+    int capacity;
+    int size;
+    float load;
+    
+
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        set = new Node[10000];
+        capacity = 10000;
+        size = 0;
+        load = size / capacity;
+    }
+    
+    public void add(int key) {
+        
+        if (contains(key)) {
+            return;
+        };
+        
+        Node n = new Node(key);
+        size++;
+        load = size/capacity;
+        
+        if (load > .7) {
+            expand();
+        }
+        
+        int hash = key % capacity;
+        
+        if (set[hash] == null) set[hash] = n;
+        else {
+            Node temp = set[hash];
+            n.next = temp;
+            set[hash] = n;
+        }
+    }
+    
+    public void expand(){
+        
+        Node[] newset = new Node[capacity * 2 + 1];
+        capacity = capacity * 2 + 1;
+        for (int i = 0; i < set.length; i++){
+            Node curr = set[i];
+            
+            while (curr != null){
+                Node next = curr.next;
+                int val = curr.value;
+                int pos = val % capacity;
+                if (newset[pos] == null) newset[pos] = curr;
+                else {
+                    Node temp = newset[pos];
+                    curr.next = temp;
+                    newset[pos] = curr;
+                }
+                curr = next;
+            }
+        }
+        
+        set = newset;
+        load = size/capacity;
+        
+    }
+    
+    public void remove(int key) {
+        
+        int pos = key % capacity;
+        Node n = set[pos];
+        if (n == null) return;
+        Node prev = n;
+        
+        if (n.value == key){   
+            set[pos] = n.next;
+        }
+        n = n.next;
+        
+        while (n != null){
+            if (n.value == key){
+                prev.next = n.next;
+            }
+            n = n.next;
+            prev = prev.next;
+        }
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        int pos = key % capacity;
+        Node n = set[pos];
+        
+        while (n != null){
+            if (n.value == key) return true;
+            n = n.next;
+        }
+        
+        return false;
+    }
+}
+
